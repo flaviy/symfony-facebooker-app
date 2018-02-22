83b47f113cda91cd84aa086d19a7dafcbe0438ec
oleksandr.chebernin <lfiecbr1>
2018-02-22 18:23:33 +0100

    twig and templates

--- src/Controller/FbPagesController.php
+++ src/Controller/FbPagesController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class FbPagesController
+class FbPagesController extends AbstractController
 {
     /**
      * @Route("/")
@@ -18,12 +19,21 @@ class FbPagesController
     }
 
     /**
-     * @Route("/show/{name}")
+     * @Route("/check_page/{name}")
      * @param $name
      * @return Response
      */
-    public function show($name)
+    public function check($name)
     {
-        return new Response('This is '.$name);
+        $posts = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('fb_pages/check.html.twig', [
+            'name' => ucwords(str_replace('-', ' ', $name)),
+            'posts' => $posts
+        ]);
     }
 }
